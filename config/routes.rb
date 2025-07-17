2940b422a89f06979c9ce4a2103789ad689bfc95
ItsukiArakaki268 <itukiprog@gmail.com>
2025-07-17 23:29:20 +0900

    add: ジャンルをランダムで返すAPIを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :food_genres, only: [:index, :show]
+      resources :food_genres, only: [:index, :show] do
+        collection do
+          get :random
+        end
+      end
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
