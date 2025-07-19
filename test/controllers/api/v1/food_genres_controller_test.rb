6613d057815a8acb18e4b1ac49b523ed6514f103
mimori37 <86526029+mimori37@users.noreply.github.com>
2025-07-20 05:41:23 +0900

    fix: テストファイルのURLヘルパーを修正
    - api_v1_food_genres_index_url → api_v1_food_genres_url
    - api_v1_food_genres_show_url → api_v1_food_genre_url(1)
    - home_index_url → root_url

--- test/controllers/api/v1/food_genres_controller_test.rb
+++ test/controllers/api/v1/food_genres_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class Api::V1::FoodGenresControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get api_v1_food_genres_index_url
+    get api_v1_food_genres_url
     assert_response :success
   end
 
   test "should get show" do
-    get api_v1_food_genres_show_url
+    get api_v1_food_genre_url(1)
     assert_response :success
   end
 end
