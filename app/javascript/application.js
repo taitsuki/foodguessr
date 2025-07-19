3dc16818f97876b40610d825da3638e0205a3e88
ItsukiArakaki268 <itukiprog@gmail.com>
2025-07-19 15:48:22 +0900

    fix: MSWを無効化

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,26 +4,33 @@ import App from "./App";
 
 console.log("application.js is loading!"); // ★これを追加
 
-async function enableMocking() {
-  console.log("enableMocking function called!"); // ★これを追加
+// async function enableMocking() {
+//   console.log("enableMocking function called!"); // ★これを追加
 
-  console.log("Importing MSW worker..."); // ★これを追加
-  // MSWを起動させるためのファイルをインポート
-  const { worker } = await import("./mocks/browser");
+//   console.log("Importing MSW worker..."); // ★これを追加
+//   // MSWを起動させるためのファイルをインポート
+//   const { worker } = await import("./mocks/browser");
 
-  console.log("Starting MSW worker..."); // ★これを追加
-  // MSWを起動
-  return worker.start();
+//   console.log("Starting MSW worker..."); // ★これを追加
+//   // MSWを起動
+//   return worker.start();
+// }
+
+// // 上記の関数を実行し、完了したらアプリの他の処理を開始
+// enableMocking().then(() => {
+//   console.log("MSW enabled!");
+
+//   // ここからReactアプリのマウント処理
+//   const container = document.getElementById("root");
+//   if (container) {
+//     const root = createRoot(container);
+//     root.render(<App />);
+//   }
+// });
+
+// MSWを無効化してReactアプリのみを起動
+const container = document.getElementById("root");
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
 }
-
-// 上記の関数を実行し、完了したらアプリの他の処理を開始
-enableMocking().then(() => {
-  console.log("MSW enabled!");
-
-  // ここからReactアプリのマウント処理
-  const container = document.getElementById("root");
-  if (container) {
-    const root = createRoot(container);
-    root.render(<App />);
-  }
-});
