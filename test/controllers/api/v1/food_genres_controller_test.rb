bf8c2d028e0794e23c0dfbe7a7b037aa15d54cd3
mimori37 <86526029+mimori37@users.noreply.github.com>
2025-07-20 05:50:32 +0900

    fix: テストで実際に存在するIDを使用するように修正
    - showアクションのテストで、実際に存在するFoodGenreのIDを使用
    - テストが正常に実行されるようになる

--- test/controllers/api/v1/food_genres_controller_test.rb
+++ test/controllers/api/v1/food_genres_controller_test.rb
@@ -7,7 +7,9 @@ class Api::V1::FoodGenresControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get show" do
-    get api_v1_food_genre_url(1)
+    # 実際に存在するIDを使用
+    food_genre = FoodGenre.first
+    get api_v1_food_genre_url(food_genre.id)
     assert_response :success
   end
 end
