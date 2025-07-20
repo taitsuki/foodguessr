4b009906096bf50701919e6324f733d0b59b3d5b
mimori37 <86526029+mimori37@users.noreply.github.com>
2025-07-20 14:30:41 +0900

    Fix string literals: change single quotes to double quotes

--- app/controllers/api/v1/food_genres_controller.rb
+++ app/controllers/api/v1/food_genres_controller.rb
@@ -75,7 +75,7 @@ class Api::V1::FoodGenresController < ApplicationController
     return false if genre.name.blank?
 
     # MyString、MyTextなどの不正なデータをチェック
-    invalid_patterns = [ "MyString", "MyText", "null", "undefined" ]
+    invalid_patterns = ["MyString", "MyText", "null", "undefined"]
     return false if invalid_patterns.any? { |pattern| genre.name.include?(pattern) }
 
     if genre.description.present?
